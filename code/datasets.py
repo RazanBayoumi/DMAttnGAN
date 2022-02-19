ff7ad13b9fc4610297cad70552405003c6f8b90b
RazanBayoumi <bayoumirazan@gmail.com>
2022-02-19 13:18:42 +0200

    Update datasets.py

--- code/datasets.py
+++ code/datasets.py
@@ -120,12 +120,12 @@ class TextDataset(data.Dataset):
 
     def load_bbox(self):
         data_dir = self.data_dir
-        bbox_path = os.path.join(data_dir, 'CelebaHQ_2022/bounding_boxes.txt')
+        bbox_path = os.path.join(data_dir, 'CelebA_500_2020/bounding_boxes.txt')
         df_bounding_boxes = pd.read_csv(bbox_path,
                                         delim_whitespace=True,
                                         header=None).astype(int)
         #
-        filepath = os.path.join(data_dir, 'CelebaHQ_2022/images.txt')
+        filepath = os.path.join(data_dir, 'CelebA_500_2020/images.txt')
         df_filenames = \
             pd.read_csv(filepath, delim_whitespace=True, header=None)
         filenames = df_filenames[1].tolist()
@@ -133,7 +133,7 @@ class TextDataset(data.Dataset):
         #
         filename_bbox = {img_file[:-4]: [] for img_file in filenames}
         numImgs = len(filenames)
-        for i in xrange(0, numImgs):
+        for i in range(0, numImgs):
             # bbox = [x-left, y-top, width, height]
             bbox = df_bounding_boxes.iloc[i][1:].tolist()
 
@@ -145,9 +145,10 @@ class TextDataset(data.Dataset):
     def load_captions(self, data_dir, filenames):
         all_captions = []
         for i in range(len(filenames)):
-            cap_path = '%s/text/%s.txt' % (data_dir, filenames[i])
+            name = filenames[i].rsplit('\r')[0]
+            cap_path = '%s/text/%s.txt' % (data_dir, name)
             with open(cap_path, "r") as f:
-                captions = f.read().decode('utf8').split('\n')
+                captions = f.read().split('\n')
                 cnt = 0
                 for cap in captions:
                     if len(cap) == 0:
@@ -288,12 +289,12 @@ class TextDataset(data.Dataset):
 
     def __getitem__(self, index):
         #
-        key = self.filenames[index]
+        key = self.filenames[index].rsplit('\r')[0]
         cls_id = self.class_id[index]
         #
         if self.bbox is not None:
             bbox = self.bbox[key]
-            data_dir = '%s/CelebaHQ_2022' % self.data_dir
+            data_dir = '%s/CelebA_500_2020' % self.data_dir
         else:
             bbox = None
             data_dir = self.data_dir
